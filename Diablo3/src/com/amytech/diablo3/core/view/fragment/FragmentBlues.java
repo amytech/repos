86ead81d62d96d3d351b3ed4211ff73b63f30b47
AmyTech <448601869@qq.com>
2015-06-02 15:07:16 +0800

    完成主界面设计。

---
+++ Diablo3/src/com/amytech/diablo3/core/view/fragment/FragmentBlues.java
@@ -0,0 +1,19 @@
+package com.amytech.diablo3.core.view.fragment;
+
+import com.amytech.library.core.framework.BaseFragment;
+
+public class FragmentBlues extends BaseFragment {
+
+	@Override
+	protected int getLayoutID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void showToUser() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
