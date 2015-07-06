a0b13eb8eaab161a939f8a47bf46c22a69692e65
amytech <448601869@qq.com>
2015-07-06 17:55:21 +0800

    add default button style.

---
+++ Diablo3Helper/src/com/amytech/diablo3helper/view/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.amytech.diablo3helper.view;
+
+import com.amytech.android.library.BaseActivity;
+import com.amytech.diablo3helper.R;
+
+/**
+ * Title: Diablo3Helper <br>
+ * Description: <br>
+ * Copyright: Copyright (c) 2015 <br>
+ * Create DateTime: 2015年7月6日 下午5:51:57 <br>
+ * SVN create person: marktlzhai <br>
+ * SVN create DateTime: 2015年7月6日 <br>
+ *
+ * @author marktlzhai
+ */
+public class HomeActivity extends BaseActivity {
+
+	@Override
+	protected int getLayoutID() {
+		return R.layout.activity_home;
+	}
+
+	@Override
+	protected void initViews() {
+
+	}
+}
--- Diablo3Helper/src/com/amytech/diablo3helper/view/SplashActivity.java
+++ Diablo3Helper/src/com/amytech/diablo3helper/view/SplashActivity.java
@@ -1,9 +1,15 @@
 package com.amytech.diablo3helper.view;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 import com.amytech.android.library.BaseActivity;
 import com.amytech.diablo3helper.R;
 
 public class SplashActivity extends BaseActivity {
+
+	private Button enterButton;
 
 	@Override
 	protected int getLayoutID() {
@@ -12,6 +18,12 @@ public class SplashActivity extends BaseActivity {
 
 	@Override
 	protected void initViews() {
-
+		enterButton = (Button) findViewById(R.id.splash_enter_btn);
+		enterButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(HomeActivity.class);
+				finish();
+			}
+		});
 	}
 }
