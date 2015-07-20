5f1fcb169c460cac9f9a02ca85ebab8166b6e916
amytech <448601869@qq.com>
2015-07-20 16:48:39 +0800

    add AmyUMengAnalytics.

---
+++ AmyUMengAnalytics/src/com/amytech/umeng/analytics/UMengAnalytic.java
@@ -0,0 +1,14 @@
+package com.amytech.umeng.analytics;
+/**
+ * Title: AmyUMengAnalytics <br>
+ * Description: <br>
+ * Copyright: Copyright (c) 2015 <br>
+ * Create DateTime: 2015年7月20日 下午4:45:40 <br>
+ * SVN create person: marktlzhai <br>
+ * SVN create DateTime: 2015年7月20日 <br>
+ *
+ * @author marktlzhai
+ */
+public class UMengAnalytic {
+
+}
