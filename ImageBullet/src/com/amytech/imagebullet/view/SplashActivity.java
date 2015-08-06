4443e9e160a1904034d15fc5e9d13b92dffc7891
amytech <448601869@qq.com>
2015-08-06 10:30:18 +0800

    remove login.

--- ImageBullet/src/com/amytech/imagebullet/view/SplashActivity.java
+++ ImageBullet/src/com/amytech/imagebullet/view/SplashActivity.java
@@ -1,8 +1,20 @@
 package com.amytech.imagebullet.view;
 
+import java.io.File;
+import java.util.List;
+
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
+import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.GetListener;
+import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UploadFileListener;
 
 import com.amytech.android.library.base.BaseActivity;
 import com.amytech.imagebullet.R;
@@ -25,6 +37,20 @@ public class SplashActivity extends BaseActivity implements OnClickListener {
 
 		loginQQButton.setOnClickListener(this);
 		enterLookButton.setOnClickListener(this);
+
+		BmobQuery<test_upload_file> query = new BmobQuery<test_upload_file>();
+		query.getObject(this, "63c9cd1df7", new GetListener<test_upload_file>() {
+
+			@Override
+			public void onSuccess(test_upload_file obj) {
+				showToast(obj.getFile().getFilename() + "\n" + obj.getFile().getUrl() + "\n" + obj.getFile().getFileUrl(SplashActivity.this));
+			}
+
+			@Override
+			public void onFailure(int arg0, String arg1) {
+				showToast("failure, " + arg0 + "\n" + arg1);
+			}
+		});
 	}
 
 	@Override
@@ -39,6 +65,24 @@ public class SplashActivity extends BaseActivity implements OnClickListener {
 			startActivity(HomeActivity.class);
 			finish();
 			break;
+		}
+	}
+
+	class test_upload_file extends BmobObject {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 7489996398428424665L;
+
+		private BmobFile file;
+
+		public BmobFile getFile() {
+			return file;
+		}
+
+		public void setFile(BmobFile file) {
+			this.file = file;
 		}
 	}
 }
